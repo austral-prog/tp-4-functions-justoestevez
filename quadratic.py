48f32d744512b0a140e58e33eef701c4de457374
justoestevez <jeestevez@mail.austral.edu.ar>
2026-03-31 12:31:42 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,7 +1,7 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    discriminante = b**2 - 4 * a * c #la raiz en la discriminante RAICES
+    discriminante = b**2 - 4 * a * c #la raiz en la discriminante RAICES BIIIIEN
     if discriminante < 0:
         return "( )"
     elif discriminante == 0:
@@ -16,9 +16,30 @@ def value_y(a, b, c, x):
     return a * x**2 + b * x + c                          #aX2 + bX + c = 0 ESTE ESTA BIEN
 
 
-def to_string(a, b, c):
-    return f"f(x) = {a} * X^2 + {b} * X + {c}"           #"f(x) = A * X^2 + B * X + C"
+def to_string(a, b, c):                                     #"f(x) = A * X^2 + B * X + C"
+    if a != 0 and b != 0 and c != 0:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
+    elif a == 0 and b == 0:
+        return f"f(x) = {c}"
+    elif a == 0 and c == 0:
+        return f"f(x) = {b} * X"
+    elif b == 0 and c == 0:
+        return f"f(x) = {a} * X^2"
+    elif a == 0:
+        return f"f(x) = {b} * X + {c}"
+    elif b == 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    elif c == 0:
+        return f"f(x) = {a} * X^2 + {b} * X"
+  
 
+def derivation(a, b, c):                                #hay un lio con los creos 000
+    if a != 0 and b != 0:
+        return f"f'(x) = {2 * a}x + {b}"
+    elif a == 0 and b == 0:
+        return f" f'(x) = 0"
+    elif a == 0:
+        return f" f'(x) = {b}"
+    elif b == 0:
+        return f" f'(x) = {2*a} * X"
 
-def derivation(a, b, c):
-    return f"f'(x) = {2*a} * X + {b}"
